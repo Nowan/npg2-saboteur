24520d05570f9fd8e5f18ad1ffcc5870acab0b98
Mykyta Dniprovskyi <mykyta@flowuse.com>
2016-06-02 22:43:15 +0200

    remake spritesheets

--- core/modules/AllySoldier.lua
+++ core/modules/AllySoldier.lua
@@ -5,52 +5,32 @@
 ]]--
 module(..., package.seeall);
 
-local sheetOptions =
-{
-    width = 200,
-    height = 150,
-    numFrames = 23
-}
 
-local spriteSheet = graphics.newImageSheet( "core/assets/sprites/enemy.png", sheetOptions );
+local sheetData1 = { width=50, height=62, numFrames=5 };
+local sheet1 = graphics.newImageSheet( "core/assets/sprites/enemy-sitting.png", sheetData1 );
+local sheetData2 = { width=40, height=58, numFrames=3 };
+local sheet2 = graphics.newImageSheet( "core/assets/sprites/enemy-standing-up.png", sheetData2 );
+local sheetData3 = { width=80, height=60, numFrames=14 };
+local sheet3 = graphics.newImageSheet( "core/assets/sprites/enemy-firing.png", sheetData3 );
+local sheetData4 = { width=52, height=60, numFrames=8 };
+local sheet4 = graphics.newImageSheet( "core/assets/sprites/enemy-reloading.png", sheetData4 );
 
--- sequences table
-local sheetSequences = {
-    -- first sequence (consecutive frames)
-    {
-        name = "sitting",
-        start = 2,
-        count = 4,
-        time = 950,
-        loopCount = 0
-    },
-    -- next sequence (non-consecutive frames)
-    {
-        name = "standingUp",
-        --frames = { 1,3,5,7 },
-        start = 5,
-        count = 5,
-        time = 550,
-        loopCount = 1
-    },
-    {
-        name = "firing",
-        --frames = { 1,3,5,7 },
-        start = 9,
-        count = 9,
-        time = 500,
-        loopCount = 0
-    },
+local sequenceData = {
+	{ name="sitting", sheet=sheet1, start=1, count=4, time=450, loopCount=0, loopDirection="bounce" },
+	{ name="standingUp", sheet=sheet2, start=1, count=3, time=450, loopCount=0 },
+	{ name="firing", sheet=sheet3, start=1, count=14, time=1050, loopCount=0 },
+	{ name="reloading", sheet=sheet4, start=2, count=8, time=1050, loopCount=0 }
 }
 
 function new()
 	local allySoldier = {};
 
-	local sprite = display.newSprite( spriteSheet, sheetSequences );
+	local sprite = display.newSprite( sheet1, sequenceData );
+	sprite.xScale = -2;
+	sprite.yScale = 2;
 	sprite.x = display.contentCenterX;
 	sprite.y = display.contentCenterY;
-	sprite.xScale = -1;
-	sprite:setSequence( "firing" );
+	sprite:setSequence( "reloading" );
 	sprite:play();
 	print("Ally spawned");
 	return allySoldier;
