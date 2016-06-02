3af2ad16f1e222dcf2130e36249b054f38731682
Mykyta Dniprovskyi <mykyta@flowuse.com>
2016-06-02 20:09:26 +0200

    add GameScreen

--- main.lua
+++ main.lua
@@ -1,7 +1,23 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--[[
 
--- Your code here
+    Start point of an application
+
+]]--
+
+------------------------------- global parameters -----------------------------------
+
+content = {};
+content.width = display.contentWidth;
+content.height = display.contentHeight;
+content.centerX = display.contentCenterX;
+content.centerY = display.contentCenterY;
+
+----------------------------------- defaults ----------------------------------------
+
+display.setDefault( "anchorX", 0 );
+display.setDefault( "anchorY", 0 );
+
+--------------------------------- starter screen -----------------------------------
+local composer = require("composer");
+
+composer.gotoScene("core.GameScene");
